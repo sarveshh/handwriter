34ea44f86805b2c6ebc3ff0cd14bc2bc3b0ae12f
Sarvesh Patil <sarveshp273@gmail.com>
2020-10-01 18:15:39 +0530

    Assignment work finalised

--- js/app.mjs
+++ js/app.mjs
@@ -1,114 +1,121 @@
-import { addFontFromFile, formatText } from './utils/helpers.mjs';
-import { generateImages, downloadAsPDF } from './generate-images.mjs';
-import { setInkColor, toggleDrawCanvas } from './utils/draw.mjs';
+import { addFontFromFile, formatText } from "./utils/helpers.mjs";
+import { generateImages, downloadAsPDF } from "./generate-images.mjs";
+import { setInkColor, toggleDrawCanvas } from "./utils/draw.mjs";
 
-
-const pageEl = document.querySelector('.page-a');
+const pageEl = document.querySelector(".page-a");
 
 const setTextareaStyle = (attrib, v) => (pageEl.style[attrib] = v);
 
 const EVENT_MAP = {
-  '#generate-image-form': {
-    on: 'submit',
+  "#generate-image-form": {
+    on: "submit",
     action: (e) => {
       e.preventDefault();
       generateImages();
-    }
+    },
   },
-  '#handwriting-font': {
-    on: 'change',
+  "#handwriting-font": {
+    on: "change",
     action: (e) =>
-      document.body.style.setProperty('--handwriting-font', e.target.value)
+      document.body.style.setProperty("--handwriting-font", e.target.value),
   },
-  '#font-size': {
-    on: 'change',
-    action: (e) => setTextareaStyle('fontSize', e.target.value + 'pt')
+  "#font-size": {
+    on: "change",
+    action: (e) => setTextareaStyle("fontSize", e.target.value + "pt"),
   },
-  '#letter-spacing': {
-    on: 'change',
-    action: (e) => setTextareaStyle('letterSpacing', e.target.value + 'pt')
+  "#letter-spacing": {
+    on: "change",
+    action: (e) => setTextareaStyle("letterSpacing", e.target.value + "pt"),
   },
-  '#word-spacing': {
-    on: 'change',
-    action: (e) => setTextareaStyle('wordSpacing', e.target.value + 'px')
+  "#word-spacing": {
+    on: "change",
+    action: (e) => setTextareaStyle("wordSpacing", e.target.value + "px"),
   },
-  '#top-padding': {
-    on: 'change',
+  "#top-padding": {
+    on: "change",
     action: (e) => {
-      document.querySelector('.page-a .paper-content').style.paddingTop =
-        e.target.value + 'px';
-    }
+      document.querySelector(".page-a .paper-content").style.paddingTop =
+        e.target.value + "px";
+    },
   },
-  '#font-file': {
-    on: 'change',
-    action: (e) => addFontFromFile(e.target.files[0])
+  "#font-file": {
+    on: "change",
+    action: (e) => addFontFromFile(e.target.files[0]),
   },
-  '#ink-color': {
-    on: 'change',
+  "#ink-color": {
+    on: "change",
     action: (e) => {
-      document.body.style.setProperty('--ink-color', e.target.value);
+      document.body.style.setProperty("--ink-color", e.target.value);
       setInkColor(e.target.value);
-    }
+    },
   },
-  '#canvas-background': {
-    on: 'change',
+  "#canvas-background": {
+    on: "change",
     action: (e) => {
-      document.querySelector('.page-a').style.setProperty('--background-img', e.target.value);
-      if ($(this).val() != '#fff') {
+      document
+        .querySelector(".page-a")
+        .style.setProperty("--background-img", e.target.value);
+      if ($(this).val() != "#fff") {
         $("#hidden-div").show();
       } else {
         $("#hidden-div").hide();
       }
-    }
+    },
   },
-  '#canvas-style': {
-    on: 'change',
+  "#canvas-style": {
+    on: "change",
     action: (e) => {
-      document.querySelector('.page-a').style.setProperty('--background-img', e.target.value)
-    }
+      document.querySelector(".page-a").style.setProperty("--background-img", e.target.value);
+    },
   },
-  '#paper-margin-toggle': {
-    on: 'change',
+  "#margin-color": {
+    on: "change",
+    action: (e) => {
+      document.querySelector(".margined").style.setProperty("--border-bottom", e.target.value);
+    },
+  },
+  "#paper-margin-toggle": {
+    on: "change",
     action: () => {
-      if (pageEl.classList.contains('margined')) {
-        pageEl.classList.remove('margined');
+      if (pageEl.classList.contains("margined")) {
+        pageEl.classList.remove("margined");
       } else {
-        pageEl.classList.add('margined');
+        pageEl.classList.add("margined");
       }
-    }
+    },
   },
-  '#paper-line-toggle': {
-    on: 'change',
+  "#paper-line-toggle": {
+    on: "change",
     action: () => {
-      if (pageEl.classList.contains('lines')) {
-        pageEl.classList.remove('lines');
+      if (pageEl.classList.contains("lines")) {
+        pageEl.classList.remove("lines");
       } else {
-        pageEl.classList.add('lines');
+        pageEl.classList.add("lines");
       }
-    }
+    },
   },
-  '#draw-diagram-button': {
-    on: 'click',
+  "#draw-diagram-button": {
+    on: "click",
     action: () => {
       toggleDrawCanvas();
-    }
+    },
   },
-  '.draw-container .close-button': {
-    on: 'click',
+  ".draw-container .close-button": {
+    on: "click",
     action: () => {
       toggleDrawCanvas();
-    }
+    },
   },
-  '#download-as-pdf-button': {
-    on: 'click',
+  "#download-as-pdf-button": {
+    on: "click",
     action: () => {
       downloadAsPDF();
-    }
+    },
   },
-  '.page-a .paper-content': {
-    on: 'paste',
-    action: formatText
-  }
+  ".page-a .paper-content": {
+    on: "paste",
+    action: formatText,
+  },
 };
 
 for (const eventSelector in EVENT_MAP) {
@@ -120,18 +127,18 @@ for (const eventSelector in EVENT_MAP) {
     );
 }
 
-document.querySelectorAll('.switch-toggle input').forEach((toggleInput) => {
-  toggleInput.addEventListener('change', (e) => {
+document.querySelectorAll(".switch-toggle input").forEach((toggleInput) => {
+  toggleInput.addEventListener("change", (e) => {
     if (toggleInput.checked) {
       document.querySelector(
         `label[for="${toggleInput.id}"] .status`
-      ).textContent = 'on';
-      toggleInput.setAttribute('aria-checked', true);
+      ).textContent = "on";
+      toggleInput.setAttribute("aria-checked", true);
     } else {
-      toggleInput.setAttribute('aria-checked', false);
+      toggleInput.setAttribute("aria-checked", false);
       document.querySelector(
         `label[for="${toggleInput.id}"] .status`
-      ).textContent = 'off';
+      ).textContent = "off";
     }
   });
 });

a932891d27dcf8ec00acf266b53ee8aab9a8796d
Sarvesh Patil <sarveshp273@gmail.com>
2020-11-01 01:11:30 +0530

    Removed route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var path = require('path')
 var cookieParser = require("cookie-parser");
+var Tesseract = require("tesseract.js");
 
-var indexRouter = require("./routes/index");
+var multer = require("multer");
 
 var app = express()
 
@@ -14,10 +15,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use("/", indexRouter);
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, res, cb) => {
+    cb(null, res.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
-app.get('/', async (req, res) => {
-  res.render('index')
-})
+app.get("/", async function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+app.post("/detect", upload.single("avatar"), function (req, res, next) {
+  const image = path.resolve(__dirname, "./" + req.file.path);
+  console.log(req.file);
+  console.log(image);
+  try {
+    Tesseract.recognize(image, `eng`, {
+      logger: (m) => console.log(m),
+      tessjs_create_pdf: "1",
+    }).then(({ data: { text } }) => {
+      console.log(text);
+      res.send({ result: text });
+    });
+  } catch (error) {
+    res.send({ result: error });
+  }
+});
 
 app.listen(process.env.PORT || 5000);

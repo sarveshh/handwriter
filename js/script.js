3ddaf3db19da80482f1cdd00ec551b0b6d0a4d9f
Sarvesh Patil <sarveshp273@gmail.com>
2020-11-01 11:12:32 +0530

    Immports and js fixed

--- js/script.js
+++ js/script.js
@@ -1,59 +1,96 @@
 $(function () {
-    $(document).on("change", ".uploadFile", function () {
-      var uploadFile = $(this);
-      var files = !!this.files ? this.files : [];
-      if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
-  
-      if (/^image/.test(files[0].type)) {
-        // only image file
-        var reader = new FileReader(); // instance of the FileReader
-        reader.readAsDataURL(files[0]); // read the local file
-  
-        reader.onloadend = function () {
-          // set image data as background of div
-          //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
-          uploadFile
-            .closest(".imgUp")
-            .find(".imagePreview")
-            .css("background-image", "url(" + this.result + ")");
-        };
-      }
-    });
-    $("#loading-modal").modal("hide");
-  
-    $("#my-form").submit(function (e) {
-      e.preventDefault();
-      $("#loading-modal").modal("show");
-      var formData = new FormData(this);
-      console.log(formData);
-      $.ajax({
-        type: "POST",
-        url: "/detect",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (r) {
-          console.log("result", r);
-          document.getElementsByTagName("pre")[0].innerHTML = r.result;
-          $("#loading-modal").modal("hide");
-        },
-        error: function (e) {
-          console.log("some error", e);
-        },
-      });
+  $(document).on("change", ".uploadFile", function () {
+    var uploadFile = $(this);
+    var files = !!this.files ? this.files : [];
+    if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+
+    if (/^image/.test(files[0].type)) {
+      // only image file
+      var reader = new FileReader(); // instance of the FileReader
+      reader.readAsDataURL(files[0]); // read the local file
+
+      reader.onloadend = function () {
+        // set image data as background of div
+        //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+        uploadFile
+          .closest(".imgUp")
+          .find(".imagePreview")
+          .css("background-image", "url(" + this.result + ")");
+      };
+    }
+  });
+  $("#loading-modal").modal("hide");
+
+  $("#my-form").submit(function (e) {
+    e.preventDefault();
+    $("#loading-modal").modal("show");
+    var formData = new FormData(this);
+    console.log(formData);
+    $.ajax({
+      type: "POST",
+      url: "/detect",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function (r) {
+        console.log("result", r);
+        document.getElementsByTagName("pre")[0].innerHTML = r.result;
+        $("#loading-modal").modal("hide");
+      },
+      error: function (e) {
+        console.log("some error", e);
+      },
     });
   });
-  
-  function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+});
+
+function copyToClipboard(text) {
+  var dummy = document.createElement("textarea");
+  // to avoid breaking orgain page when copying more words
+  // cant copy when adding below this code
+  // dummy.style.display = 'none'
+  document.body.appendChild(dummy);
+  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
+
+function toggleTheme(toggleButton) {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("fade-in-light");
+    document.body.classList.remove("dark");
+    document.body.classList.remove("fade-in-dark");
+    window.localStorage.setItem("prefers-theme", "light");
+    if (toggleButton) {
+      toggleButton.setAttribute("aria-pressed", false);
+      toggleButton.setAttribute("aria-label", "Activate Dark Mode");
+    }
+  } else {
+    document.body.classList.add("fade-in-dark");
+    document.body.classList.add("dark");
+    document.body.classList.remove("fade-in-light");
+    window.localStorage.setItem("prefers-theme", "dark");
+    if (toggleButton) {
+      toggleButton.setAttribute("aria-pressed", true);
+      toggleButton.setAttribute("aria-label", "Activate Light Mode");
+    }
   }
-  
+}
+
+const localPreference = window.localStorage.getItem("prefers-theme");
+if (localPreference) {
+  if (localPreference === "light") document.body.classList.remove("dark");
+  else document.body.classList.add("dark");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.body.classList.add("dark");
+}
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}

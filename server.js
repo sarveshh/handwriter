df5c9f457a40f97a259d1167967f6c16e8155cbe
Sarvesh Patil <sarveshp273@gmail.com>
2020-11-04 10:59:15 +0530

    Fixed draw error

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('prerender-node'));
 app.use(express.static(__dirname + "/"));
 app.set("views", path.join(__dirname, "views"));
 app.engine("html", require("ejs").renderFile);
